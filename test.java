fd3048f525bdaeeb7ba8b788a4dbb80afa359c29
Słuchacz <Suchacz>
2019-03-23 10:15:23 +0100

    Added String

--- test.java
+++ test.java
@@ -1,10 +1,14 @@
-/*
+/* GIT CLONE "NAZWA PLIKU"
 1) ZMIANA W PLIKU
 2) GIT STATUS <- Status workspace
 3) GIT ADD <- Dodaje do indeksu
 4) GIT COMMIT <- Dodaje do lokalne REPO
 5) GIT PUSH <- Dodaje do zdalnego REPO
+------------------
+1) GIT BRANCH	______ utwarza plik tak jakby (--list ukazuje list)
+2) GIT CHECKOUT	______ przechodzi do tego pliku
 
+//git push -u origin test
 */
 
 
@@ -13,4 +17,7 @@ public class test {
 	public static void main(String[]args) {
 		System.out.println("HelloWorld");
 	}
+	public String test() {
+		return "test";	
+		}
 }
