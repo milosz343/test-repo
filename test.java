c62a46a0b5675b73d090effe6df1bb25ac8acaea
Słuchacz <Suchacz>
2019-03-23 09:56:20 +0100

    Added println and comment

--- test.java
+++ test.java
@@ -1,5 +1,16 @@
+/*
+1) ZMIANA W PLIKU
+2) GIT STATUS <- Status workspace
+3) GIT ADD <- Dodaje do indeksu
+4) GIT COMMIT <- Dodaje do lokalne REPO
+5) GIT PUSH <- Dodaje do zdalnego REPO
+
+*/
+
+
+
 public class test {
 	public static void main(String[]args) {
-		//Hello World
+		System.out.println("HelloWorld");
 	}
 }
